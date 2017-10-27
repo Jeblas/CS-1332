3bdec2c6068a7d9c0bd041872067712b9d00938b
John B <Jeblas@users.noreply.github.com>
2017-10-27 15:03:23 -0400

    Add files via upload

---
+++ Sorting Algorithms/Sorting.java
@@ -0,0 +1,174 @@
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ * Your implementation of various sorting algorithms.
+ *
+ * @author YOUR NAME HERE
+ * @version 1.0
+ */
+public class Sorting {
+
+    /**
+     * Implement insertion sort.
+     *
+     * It should be:
+     *  in-place
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(n^2)
+     *
+     * And a best case running time of:
+     *  O(n)
+     *
+     * Any duplicates in the array should be in the same relative position after
+     * sorting as they were before sorting. (stable).
+     *
+     * See the PDF for more info on this sort.
+     *
+     * @throws IllegalArgumentException if the array or comparator is null
+     * @param <T> data type to sort
+     * @param arr the array that must be sorted after the method runs
+     * @param comparator the Comparator used to compare the data in arr
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<T> comparator) {
+
+    }
+
+    /**
+     * Implement kth select.
+     *
+     * Use the provided random object to select your pivots.
+     * For example if you need a pivot between a (inclusive)
+     * and b (exclusive) where b > a, use the following code:
+     *
+     * int pivotIndex = r.nextInt(b - a) + a;
+     *
+     * It should be:
+     *  in-place
+     *
+     * Have a worst case running time of:
+     *  O(n^2)
+     *
+     * And a best case running time of:
+     *  O(n)
+     *
+     * Note that there may be duplicates in the array.
+     *
+     * Make sure you code the algorithm as you have been taught it in class.
+     * There are several versions of this algorithm and you may not get full
+     * credit if you do not use the one we have taught you!
+     *
+     * @throws IllegalArgumentException if the array or comparator or rand is
+     * null or k is not in the range of 1 to arr.length
+     * @param <T> data type to sort
+     * @param k the index + 1 (due to 0-indexing) to retrieve the data 
+     * from as if the array were sorted; the 'k' in "kth select"
+     * @param arr the array that should be modified after the method
+     * is finished executing as needed
+     * @param comparator the Comparator used to compare the data in arr
+     * @param rand the Random object used to select pivots
+     * @return the kth smallest element
+     */
+    public static <T> T kthSelect(int k, T[] arr, Comparator<T> comparator,
+                                     Random rand) {
+
+    }
+
+    /**
+     * Implement merge sort.
+     *
+     * It should be:
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(n log n)
+     *
+     * And a best case running time of:
+     *  O(n log n)
+     *
+     * You can create more arrays to run mergesort, but at the end,
+     * everything should be merged back into the original T[]
+     * which was passed in.
+     * 
+     * When necessary due to an odd number of elements, the 
+     * excess element MUST go on the right side!
+     *
+     * Any duplicates in the array should be in the same relative position after
+     * sorting as they were before sorting.
+     *
+     * @throws IllegalArgumentException if the array or comparator is null
+     * @param <T> data type to sort
+     * @param arr the array to be sorted
+     * @param comparator the Comparator used to compare the data in arr
+     */
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+
+    }
+
+    /**
+     * Implement LSD (least significant digit) radix sort.
+     *
+     * Remember you CANNOT convert the ints to strings at any point in your
+     * code!
+     *
+     * It should be:
+     *  stable
+     *
+     * Have a worst case running time of:
+     *  O(kn)
+     *
+     * And a best case running time of:
+     *  O(kn)
+     *
+     * Any duplicates in the array should be in the same relative position after
+     * sorting as they were before sorting. (stable)
+     *
+     * Do NOT use {@code Math.pow()} in your sort. Instead, if you need to, use
+     * the provided {@code pow()} method below.
+     *
+     * You may use {@code java.util.ArrayList} or {@code java.util.LinkedList}
+     * if you wish, but it may only be used inside radix sort and any radix sort
+     * helpers. Do NOT use these classes with other sorts.
+     *
+     * @throws IllegalArgumentException if the array is null
+     * @param arr the array to be sorted
+     * @return the sorted array
+     */
+    public static int[] lsdRadixSort(int[] arr) {
+
+    }
+
+    /**
+     * Calculate the result of a number raised to a power. Use this method in
+     * your radix sorts instead of {@code Math.pow()}.
+     *
+     * DO NOT MODIFY THIS METHOD.
+     *
+     * @throws IllegalArgumentException if both {@code base} and {@code exp} are
+     * 0
+     * @throws IllegalArgumentException if {@code exp} is negative
+     * @param base base of the number
+     * @param exp power to raise the base to. Must be 0 or greater.
+     * @return result of the base raised to that power
+     */
+    private static int pow(int base, int exp) {
+        if (exp < 0) {
+            throw new IllegalArgumentException("Exponent cannot be negative.");
+        } else if (base == 0 && exp == 0) {
+            throw new IllegalArgumentException(
+                    "Both base and exponent cannot be 0.");
+        } else if (exp == 0) {
+            return 1;
+        } else if (exp == 1) {
+            return base;
+        }
+        int halfPow = pow(base, exp / 2);
+        if (exp % 2 == 0) {
+            return halfPow * halfPow;
+        } else {
+            return halfPow * halfPow * base;
+        }
+    }
+}
